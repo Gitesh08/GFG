4ec19024a2e1dcc558a2d44025967cdab4643246
Gitesh Mahadik <115268756+Gitesh08@users.noreply.github.com>
2023-12-20 21:07:12 +0530

    Potd 20_DEC_23
    You are given an array A of n elements. There are two players player 1 and player 2.
    A player can choose any of element from an array and remove it. If the bitwise XOR of all remaining elements equals 0 after removal of the selected element, then that player loses. Find out the winner if player 1 starts the game and they both play their best.

---
+++ GFG POTD/20_DEC_23.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findWinner(self, n, A):
+        # code here
+        x0r = 0;
+        for i in range(n):    
+            x0r ^= A[i];
+        return ((n % 2) + 1 if x0r else 1);
